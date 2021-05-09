5ae3b6f1385c0162cd31bb08477623351cbfe82d
budaizalan <72376854+budaizalan@users.noreply.github.com>
2021-05-09 16:27:35 +0200

    negyzet

---
+++ negyzet.py
@@ -0,0 +1,13 @@
+def main() -> None:
+
+    a = int(input("KKérem a négyzet oldalának hosszúságát:"))
+
+    terulet = a*a
+    kerulet = 4*a
+
+    print("A négyzet területe(T):", terulet)
+    print("A négyzet kerülete(K):", kerulet)
+
+
+if __name__ == "__main__":
+    main()
